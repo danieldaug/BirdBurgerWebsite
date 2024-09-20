14927431c50c87334234af4b3c72df3b81d5cd86
danieldaug <danieldaug3@gmail.com>
2024-09-20 13:49:09 -0700

    slider module for products added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,25 @@ import './App.css';
 import SocialMediaButtons from './components/SocialMediaButtons';
 import Logo from './components/MainLogo';
 import TextSection from './components/TextSection';
+import ImageSlider from './components/ImageSlider';
+import DDIcon from './assets/img/DDIcon.png'
+import main_logo from './assets/img/main_logo.png';
+
+// Current products to display
+const imageData = [
+  {
+      imageUrl: DDIcon,
+      description: 'Our first mobile game to be released in the near future. Look out for Daily Disc on the IOS App Store!',
+  },
+  {
+      imageUrl: main_logo,
+      description: 'This is the second image',
+  },
+  {
+      imageUrl: DDIcon,
+      description: 'This is the third image',
+  },
+];
 
 function App() {
   return (
@@ -12,9 +31,13 @@ function App() {
         <Logo />
         <TextSection 
           title="Welcome to BirdBurger Games" 
-          description="We are an indie development studio. Look out for our first game coming later this year." 
+          description="We are an indie development studio. Look out for our first mobile game, Daily Disc, coming later this year." 
         />
         <SocialMediaButtons />
+        <div className="Slider">
+          <h1>Products</h1>
+          <ImageSlider images={imageData} />
+        </div>
       </header>
     </div>
   );
