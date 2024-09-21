e55da7e905e2dbe7f042b1708c2f9286461ecd39
danieldaug <danieldaug3@gmail.com>
2024-09-20 21:59:00 -0700

    reorganized meet the team module

--- src/components/ImageDisplayer.tsx
+++ src/components/ImageDisplayer.tsx
@@ -1,4 +1,3 @@
-// components/ImageDisplayer.tsx
 import React from 'react';
 import './ImageDisplayer.css';
 import { FaLinkedin, FaInstagram, FaGithub } from 'react-icons/fa';
@@ -21,35 +20,42 @@ const ImageDisplayer: React.FC<ImageDisplayerProps> = ({ images }) => {
   return (
     <div className="display-container">
       {images.map((image, index) => (
-        <div className="display-content" key={index} style={{ marginBottom: '20px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-          <img
-            src={image.imageUrl}
-            alt={image.description}
-            className="display-image"
-            style={{ borderRadius: '20px', marginBottom: '10px', width: '100%' }} // Make the image full width
-          />
-          <div className="display-text" style={{ textAlign: 'center' }}>
-            {image.header && <h1 className="display-header">{image.header}</h1>}
-            <p className="display-description">{image.description}</p>
+        <div className="display-content" key={index} style={{ marginBottom: '20px', display: 'flex' }}>
+          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+            <img
+              src={image.imageUrl}
+              alt={image.description}
+              className="display-image"
+              style={{
+                borderRadius: '20px',
+                marginRight: '20px',
+                width: '400px',  // Set a fixed width
+                height: '400px', // Set a fixed height
+                objectFit: 'cover' // Maintain aspect ratio and cover the box
+              }}
+            />
           </div>
-          <div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
-            {image.linkedin && (
-            <a className="social-buttons" key={`linkedin-${index}`} href={image.linkedin} target="_blank" rel="noopener noreferrer">
-            <FaLinkedin />
-            </a>
-            )}
-            {image.ig && (
-            <a className="social-buttons" key={`instagram-${index}`} href={image.ig} target="_blank" rel="noopener noreferrer">
-            <FaInstagram />
-            </a>
-            )}
-            {image.git && (
-            <a className="social-buttons" key={`github-${index}`} href={image.git} target="_blank" rel="noopener noreferrer">
-            <FaGithub />
-            </a>
-            )}
-        </div>
-
+          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-end' }}>
+            {image.header && <h1 className="display-header" style={{ textAlign: 'right' }}>{image.header}</h1>}
+            <p className="display-description" style={{ textAlign: 'right' }}>{image.description}</p>
+            <div style={{ display: 'flex', alignItems: 'flex-end', gap: '10px' }}>
+              {image.linkedin && (
+                <a className="social-buttons-small" key={`linkedin-${index}`} href={image.linkedin} target="_blank" rel="noopener noreferrer">
+                  <FaLinkedin />
+                </a>
+              )}
+              {image.ig && (
+                <a className="social-buttons-small" key={`instagram-${index}`} href={image.ig} target="_blank" rel="noopener noreferrer">
+                  <FaInstagram />
+                </a>
+              )}
+              {image.git && (
+                <a className="social-buttons-small" key={`github-${index}`} href={image.git} target="_blank" rel="noopener noreferrer">
+                  <FaGithub />
+                </a>
+              )}
+            </div>
+          </div>
         </div>
       ))}
     </div>
@@ -57,5 +63,3 @@ const ImageDisplayer: React.FC<ImageDisplayerProps> = ({ images }) => {
 };
 
 export default ImageDisplayer;
-
-
